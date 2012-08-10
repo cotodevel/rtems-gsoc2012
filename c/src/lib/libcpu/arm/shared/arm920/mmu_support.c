14a3c74eaf719e41fbf4ad61a04d5ed867775ed1
Hesham AL-Matary <heshamelmatary@gmail.com>
2012-08-10 21:24:05 +0200

    Implement Uninstall_MPE

--- c/src/lib/libcpu/arm/shared/arm920/mmu_support.c
+++ c/src/lib/libcpu/arm/shared/arm920/mmu_support.c
@@ -12,78 +12,16 @@
 #include <rtems/rtems/status.h>
 #include "mmu_support.h"
 
-/* Find a valid page table to map a region for it 
- * FIXME: currently a page table may map up to 1M
- * byte of memory */
-static uint32_t search_empty_PT(uint32_t base)
-{
+extern uint32_t _ttbl_base;
 
-  /*  TODO: distinguish between lvl1/lvl2 PT search
- *  according to the base address */
 
-  mmu_lvl2_t  *lvl2_ptr = (mmu_lvl2_t *) _ttbl2_base;
-
-  for ( ; lvl2_ptr < ( lvl2_index + 256 ); lvl2_ptr++ )
-    if ( *lvl2_ptr == 0x0 )
-      return lvl2_ptr;
-
-  return 0;
-}
-/* @brief Allocate a page table for one region 
- * FIXME: Specify maximum number of page table/regions here
- * and allocate memory for them 
+/* @brief Deleting MPE and freeing its Control block as well as uninstalling
+ * mpe from HW and uninstall the high-level pointer to cpu_mpe
  */
-static mmu_lvl2_t *arm_PT_Allocate(void)
+static inline rtems_status_code _CPU_Memory_management_Delete_MPE(rtems_memory_management_entry *mpe)
 {
-  /* allocate lvl2 page table, 1KB size */
-  mmu_lvl2_t *lvl2_PT_base = (mmu_lvl2_t *) malloc( 1024 ); 
 
-  if ( lvl2_PT_base == NULL )
-    return 0;
-
-  return lvl2_PT_base;
 }
-
-/* initialize page table with fault entries */
-static inline rtems_status_code arm_PT_Initialize(arm_bsp_mm_mpe *cpu_mpe)
-{ 
-  pt_type type = cpu_mpe->type;
-  uint32_t  paddr,base_paddr;
-  uint32_t *PTEptr;
-
-  if ( type == ARM_MMU_LVL2_4KB_PT ) {
-
-    base_paddr = (cpu_mpe->vAddress & 0xfff00000); /* 1:1 Mapping */
-
-    mmu_lvl2_t *PTEptr = (mmu_lvl2_t *)cpu_mpe->ptAdress;
-
-    for (i = 0; i < 256; i++) {
-
-      paddr = base_paddr + ( i << 12 ); /* i = 4K pages */
-      PTEptr[i] = MMU_SET_LVL2_PTE_4KB(paddr,
-                                       ARM_MMU_AP_NOPR,
-                                       1,
-                                       0);
-    }
-  }
-  return RTEMS_SUCCESSFUL;
-}
-
-/* Activating a region means that installing the corresponding lvl2 page table base address
- *  * at lvl1 pagetable, that is, the system is fully installed to generate exceptions and 
- *   * apply AP/CB when accessing that region according to its attributes. */
-static inline rtems_status_code arm_PT_Activate(arm_bsp_mm_mpe *);
-
-/* Add PTEs to a page table. Number of PTEs is determined according to region's size
- *  * Every page table controls one region, Installing a region means that allocating 
- *   * PTEs at CPU page tables, but not necessary activated */
-static inline rtems_status_code arm_PT_Install(arm_bsp_mm_mpe *);
-
-/* Deleting lvl2 page table base address from lvl1 page table */
-static inline rtems_status_code arm_PT_UnInstall(arm_bsp_mm_mpe *);
-
-/* Deleting a page table corresponds to delete a region */
-static inline rtems_status_code arm_PT_Delete(arm_bsp_mm_mpe *);
 
 /* Changing Page table attributes to new attributes */
 static inline rtems_status_code arm_PT_Change_Attr(arm_bsp_mm_mpe *,uint32_t AP, uint32_t CB);
@@ -185,7 +123,7 @@ rtems_status_code _CPU_Memory_management_Install_MPE(
   /* Set AP for this region to NO ACCESS */
 
   int i;
-  for ( i = 0; i < numPages; i++) {
+  for ( i = 0; i < sectionsNumber; i++) {
     uint32_t paddr_base = paddr;
     paddr_base = (i<<20) + paddr;
 
@@ -210,5 +148,86 @@ rtems_status_code _CPU_Memory_management_Install_MPE(
   /* install a pointer to high-level API to bsp_mm_mpe */
   mpe->cpu_mpe = cpu_mpe;
   
+  /* flush the cache and TLB */
+  arm_cp15_cache_invalidate();
+  arm_cp15_tlb_invalidate();
+
+  /*  I & D caches turned on */
+  arm_cp15_set_control(MMU_CTRL_DEFAULT |
+                       MMU_CTRL_D_CACHE_EN |
+                       MMU_CTRL_I_CACHE_EN |
+                       MMU_CTRL_ALIGN_FAULT_EN |
+                       MMU_CTRL_LITTLE_ENDIAN |
+                       MMU_CTRL_MMU_EN);
+  return RTEMS_SUCCESSFUL;
+}
+
+/* @brief Unistalling @mpe from level1 page table and return 
+ * access/cache attributes to its defaults. Note that bsp MPE
+ * will still exist even after uninstalling mpe 
+ */
+rtems_status_code _CPU_Memory_management_UnInstall_MPE(
+  rtems_memory_management_entry *mpe
+)
+{
+  arm_bsp_mm_mpe *cpu_mpe;
+  mmu_lvl1_t     *lvl1_pt;
+  int             sectionsNumber; /* 1MB sections */
+  size_t          size; /* per Byte */
+  uint32_t        vAddress;
+  int             PTEIndex;
+  uint32_t        paddr;
+
+  arm_mpe = (arm_bsp_mm_mpe *) mpe->cpu_mpe;
+
+  if ( cpu_mpe == NULL )
+    return RTEMS_UNSATISFIED;
+
+  sectionsNumber = arm_mpe->pagesNumber;
+  
+  lvl1_pt = (mmu_lvl1_t *) _ttbl_base;
+  PTEIndex = ((mpe->region.base & 0xfff00000) >> 20);
+  paddr = (mpe->region.base & 0xfff00000);
+  
+  /* flush the cache and TLB */
+  arm_cp15_cache_invalidate();
+  arm_cp15_tlb_invalidate();
+
+  /*  I & D caches turned off */
+  arm_cp15_set_control(MMU_CTRL_DEFAULT |
+                       MMU_CTRL_D_CACHE_DES |
+                       MMU_CTRL_I_CACHE_DES |
+                       MMU_CTRL_ALIGN_FAULT_EN |
+                       MMU_CTRL_LITTLE_ENDIAN |
+                       MMU_CTRL_MMU_DES);
+
+  /* Return AP/CB for this region to defaults */
+  int i;
+  for ( i = 0; i < sectionsNumber; i++) {
+    uint32_t paddr_base = paddr;
+    paddr_base = (i<<20) + paddr;
+
+    lvl1_pt[PTEIndex++] = MMU_SET_LVL1_SECT(paddr_base,
+                                            ARM_MMU_AP_NO_ACCESS,
+                                            0,
+                                            1,
+                                            0);
+  }
+
+
+  cpu_mpe->AP   = ARM_MMU_AP_NO_ACCESS; /* Default */
+  cpu_mpe->CB   = ARM_MMU_WT; /* Default */
+
+    /* flush the cache and TLB */
+  arm_cp15_cache_invalidate();
+  arm_cp15_tlb_invalidate();
+
+  /*  I & D caches turned on */
+  arm_cp15_set_control(MMU_CTRL_DEFAULT |
+                       MMU_CTRL_D_CACHE_EN |
+                       MMU_CTRL_I_CACHE_EN |
+                       MMU_CTRL_ALIGN_FAULT_EN |
+                       MMU_CTRL_LITTLE_ENDIAN |
+                       MMU_CTRL_MMU_EN);
   return RTEMS_SUCCESSFUL;
 }
